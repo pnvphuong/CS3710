00a13d0397807c10c8b1e4ee652faf020b2b484f
nils <nineil.cs@gmail.com>
2015-04-01 14:25:00 -0400

    learn model hierarhical Ready!

---
+++ SpatialPyramid/hierarchical/buildClassifierHierarchy.m
@@ -0,0 +1,20 @@
+function map_learned_models = buildClassifierHierarchy( map_kernels, map_trainIDList_testIDList)
+    values = {};
+    ks = keys(map_kernels);
+    for i=1:length(map_kernels)
+        k = ks{i};
+        val_kernel = map_kernels(k);
+        val_train_test = map_trainIDList_testIDList(k);
+        
+        K = val_kernel{1};
+        trainIDList = val_train_test(1);
+        
+        classifierList = buildLeafNodeClassifierList(K, trainIDList);                
+        values = [values classifierList];
+    end
+    
+    % create output map
+    M = containers.Map(ks, values);
+    map_learned_models = M;
+end
+
