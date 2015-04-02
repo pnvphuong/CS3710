622ffb4fa87c25a456c89590c82fc791343a66a3
nils <nineil.cs@gmail.com>
2015-04-01 23:34:16 -0400

    added GroundTruth Hierarchical

---
+++ SpatialPyramid/hierarchical/getGroundTruth.m
@@ -0,0 +1,19 @@
+% r = map_IDList('animal');
+% getGroundTruth( r{2} , categoryList)
+function vg = getGroundTruth( test_instances , leaves)
+vg = {};
+for i = 1: length(test_instances) % number of classes
+    class_instances = test_instances{i};
+    for j = 1: length(class_instances)
+        instance = class_instances{j};
+        for k = 1: length(leaves)
+            leaf = leaves{k};
+            if isempty(strfind(instance, leaf)) == false % found
+                vg = [vg leaf];
+            end
+        end
+    end
+end
+
+end
+
