d49da795a27664b6ff7bfa340e42fae5d82bd1ca
pnvphuong <pitt.pnvphuong@gmail.com>
2015-04-19 14:47:50 -0400

    hierarchy classifier, euclidean matrix

--- SpatialPyramid/evaluateHierarchy.m
+++ SpatialPyramid/evaluateHierarchy.m
@@ -18,13 +18,15 @@ function confusionMatrix = evaluateHierarchy(nameHierarchy)
         'snake', 'snail', 'zebra', 'greyhound', 'toad', ...
         'horseshoe-crab', 'crab', 'conch', 'dolphin', ...
         'goldfish', 'killer-whale', 'mussels', 'octopus', 'starfish'};
-    categoryIdx = linspace(1, length(categoryList), length(categoryList));
+    categoryIdx = (1 : length(categoryList));
     categoryMap = containers.Map(categoryList,categoryIdx);
 
     epoch = 40;
     epoch = 1;
     trainTestRatio = 0.3;
     
+    confusionMatrixList = cell(1,epoch);
+    accList = zeros(1,epoch);
     for i = 1 : epoch
         sprintf('%d run',i)
         % set seed random number
@@ -36,43 +38,98 @@ function confusionMatrix = evaluateHierarchy(nameHierarchy)
         [trainIDList, testIDList] = extractTrainTestList(categoryList,...
             caltechTaxonomyMap,	trainTestRatio, baseFolder);
         % build classifier
-        % predict
+        sprintf('build')
+        [hierarchyClassifier, hierarchyTrainSet] = buildHierarchyClassifier(...
+            nameHierarchy,trainIDList,categoryMap,featureMap);
+        % evaluate/predict the test set
+        sprintf('predict')
+        confusionMatrix = evaluateHierarchyClassifier(testIDList,...
+            hierarchyClassifier, hierarchyTrainSet,featureMap,nameHierarchy,...
+            categoryMap,categoryList);
+        confusionMatrixList{i} = confusionMatrix;
+        accList(i) = trace(confusionMatrix) / sum(sum(confusionMatrix));
+        save('../data/confusionMatrixHierarchy.mat', 'confusionMatrixList');
     end
+    sprintf('avg Acc=%f',sum(accList) / epoch)
 end
 
-% build hierarchy classifier using map
-function hierarchyClassifier = buildHierarchyClassifier(K,nameHierarchy)
+% build hierarchy classifier using map, i.e. a 1vsAll classifier for each
+% key of the map nameHierarchy
+function [hierarchyClassifier, hierarchyTrainSet] =...
+    buildHierarchyClassifier(nameHierarchy,trainIDList,categoryMap,featureMap)
     categoryNameKey = keys(nameHierarchy);
     classifierList = cell(1,length(categoryNameKey));
+    trainSetList = cell(size(classifierList));
     % each key contain a list of children, we build 1 vs all for these
     % children
-    for iParent = 1 : length(nameHierarchy)
-        sprintf('classifierName=%s', nameHierarchy{iParent})
-        classifierNameList = categoryNameHierarchy(nameHierarchy{iParent});
-        classifierList{iParent} = buildClassifierListFromNameList(classifierNameList,nameHierarchy);
+    for iParent = 1 : length(categoryNameKey)
+        % sprintf('classifierName=%s', categoryNameKey{iParent})
+        classifierNameList = nameHierarchy(categoryNameKey{iParent});
+        [classifierList{iParent}, trainSetList{iParent}] =...
+            buildClassifierListFromNameList(classifierNameList,...
+            nameHierarchy,trainIDList,categoryMap, featureMap);
     end
     hierarchyClassifier = containers.Map(categoryNameKey,classifierList);
+    hierarchyTrainSet = containers.Map(categoryNameKey,trainSetList);
 end
 
 % build 1 vs all classifier for this branch
 % each classifierNameList element is a 1 vs all classifier
-function classifierList = buildClassifierListFromNameList(classifierNameList,...
-    categoryNameHierarchy)
+function [classifierList, branchTrainIDList] = buildClassifierListFromNameList(...
+    classifierNameList,categoryNameHierarchy, trainIDList, categoryMap,...
+    featureMap)
     % analyze each classifierNameList element into atom category names
     individualNameList = cell(1,length(classifierNameList));
     for i = 1 : length(classifierNameList)
-        individualNameList{i} = extractCategoryFromCategoryName(classifierNameList{i},...
-            categoryNameHierarchy,{});
+        individualNameList{i} = extractCategoryFromCategoryName(...
+            classifierNameList{i}, categoryNameHierarchy,{});
+    end    
+    % get trainIDList for this branch (all children's atom names)
+    branchTrainIDList = {};    
+    for i = 1 : length(individualNameList)
+        individualName = individualNameList{i};
+        for j = 1 : length(individualName)
+            branchTrainIDList = [branchTrainIDList {trainIDList{...
+                categoryMap(individualName{j})}}];            
+        end
     end
-    % extract train, test data
-        % how to build 1 vs all automatically? buildLeafNodeClassifierList
-        % filter trainIDList
-        % compute kernel
-        sprintf('\t\tcompute kernel')
-        [K, KK, trainID, testID] = computeKernel(trainIDList,...
-            testIDList, featureMap);
-        % or only use data set within this branch? yes, recompute K for
-        % each branch
+    
+    % compute kernel
+    K = computeSingleKernel(branchTrainIDList,featureMap);
+%     save('../data/testK.mat','K');
+%   load('../data/testK.mat');    
+    % build branch 1vsall classifier
+    classifierList = buildBranch1vsAllClassifier(K,trainIDList,...
+        individualNameList,categoryMap);
+end
+
+function classifierList = buildBranch1vsAllClassifier(K,trainIDList,...
+    individualNameList, categoryMap)
+    classifierList = cell(1, length(individualNameList));
+    for i = 1 : length(classifierList)
+        classValue = getBranchClassValue(individualNameList, trainIDList,...
+            categoryMap,i);
+        classifierList{i} = buildBinaryClassifier(K, classValue);
+    end
+end
+
+function classValue = getBranchClassValue(individualNameList, trainIDList,...
+    categoryMap, iCategory)
+	classValue = [];
+	for i = 1 : length(individualNameList)
+        len = 0;
+        individualName = individualNameList{i};
+        for j = 1 : length(individualName)
+            len = len + length(trainIDList{categoryMap(individualName{j})});
+        end
+		categoryClassValue = ones(len,1);
+		if i ~= iCategory
+			% negative class
+			categoryClassValue = categoryClassValue .* -1;
+		end
+
+		classValue = [classValue; categoryClassValue];
+	end
 end
 
 function categoryList = extractCategoryFromCategoryName(categoryName,...
@@ -87,4 +144,132 @@ function categoryList = extractCategoryFromCategoryName(categoryName,...
     else % leaf node
         categoryList = [categoryList categoryName];
     end
+end
+
+function K = computeSingleKernel(imageIDList, featureMap)
+    [trainFeatureTable, trainID] = getFeatureList(imageIDList, featureMap);
+
+	k = hist_isect(trainFeatureTable, trainFeatureTable);
+	K = [ (1:size(trainFeatureTable,1))' , k ];
+end
+
+function [featureList, id] = getFeatureList(idList, featureMap)
+	id = [];
+	for i = 1 : length(idList)
+		id = [id; idList{i}];
+	end
+	id = cellstr(id);
+
+	featureList = getFeatureFromID(featureMap, id);
+end
+
+function featureList = getFeatureFromID(featureMap, idList)
+	featureList = [];
+	for i = 1 : length(idList)
+		featureList = [featureList; featureMap(idList{i})];
+	end
+end
+
+% evaluate hierarchy 
+function confusionMatrix = evaluateHierarchyClassifier(testIDList,...
+    hierarchyClassifier, hierarchyTrainSet,featureMap,nameHierarchy,...
+    categoryMap,categoryList)
+    totalTestImage = 0;
+    for i = 1 : length(testIDList)
+        totalTestImage = totalTestImage + length(testIDList{i});
+    end
+    confusionMatrix = zeros(length(categoryMap));
+    iCount = 1;
+    % different image has different hierarchy path
+    for iTest = 1 : length(testIDList)
+        subTestID = testIDList{iTest};
+        for jTest = 1 : length(subTestID)
+            sprintf('predict %d/%d image',iCount,totalTestImage)
+            testID = subTestID{jTest};            
+            predCategoryName = predictImage(testID,hierarchyClassifier,...
+                hierarchyTrainSet,featureMap,nameHierarchy);
+            testNameIdx = iTest; % use in the confusion matrix
+            % update confusion matrix
+            % row = target; col = prediction
+            confusionMatrix(testNameIdx,categoryMap(predCategoryName)) =...
+                confusionMatrix(testNameIdx,categoryMap(predCategoryName))+1;
+            iCount = iCount + 1;            
+        end
+    end
+end
+
+% predict category name for an image
+function predCategoryName = predictImage(testID,hierarchyClassifier,...
+    hierarchyTrainSet,featureMap,nameHierarchy)
+    % go through the hierarchy (recursive) by starting at the root, the
+    % longest node name
+    nodeNameList = keys(nameHierarchy);
+    nodeNameLengthList = cellfun(@length, nodeNameList);
+    rootNode = nodeNameList{find(nodeNameLengthList == max(nodeNameLengthList))};
+    % predict category name of this testID image
+    predCategoryName = predictHierarchyClassifier(testID,hierarchyClassifier,...
+        hierarchyTrainSet,rootNode,featureMap,nameHierarchy);
+end
+
+% recursive predict the image
+function predName = predictHierarchyClassifier(testID,hierarchyClassifier,...
+    hierarchyTrainSet,nodeName,featureMap,nameHierarchy)
+%     sprintf('nodeName=%s',nodeName)
+    classifierList = hierarchyClassifier(nodeName);
+    trainIDList = hierarchyTrainSet(nodeName);
+    categoryNameList = nameHierarchy(nodeName);
+    % compute kernel
+    KK = computeTestKernel(trainIDList,testID,featureMap);
+    predClass = predictMulticlassInstance(KK, classifierList);
+    [row,col] = find(predClass == 1);   
+    subNodeName = categoryNameList{col(1)}; % col has only 1 value
+    % is non-terminal?
+    if isKey(nameHierarchy,subNodeName)
+        predName = predictHierarchyClassifier(testID,hierarchyClassifier,...
+            hierarchyTrainSet,subNodeName,featureMap,nameHierarchy);
+    else
+        predName = subNodeName;
+    end
+end
+
+function KK = computeTestKernel(trainIDList, testID, featureMap)
+    [trainFeatureTable, trainID] = getFeatureList(trainIDList, featureMap);
+	k = hist_isect(featureMap(testID), trainFeatureTable);
+	KK = [ (1:size(testID,1))' , k ];
+end
+
+% predict test image, output the prediction index according to categoryName
+function predClass = predictMulticlassInstance(KK, classifierList)
+    % prediction probability on each class
+	predProb = zeros(size(KK,1), length(classifierList));
+    classVector = ones(size(predProb,1),1);
+    for iCategory = 1 : length(classifierList)
+		% get prediction probability
+		decVals = predictBinaryClassifier(KK, classVector, classifierList{iCategory});
+		% merge into the prediction matrix
+		predProb(:,iCategory) = decVals(:,find(classifierList{iCategory}.Label == 1));
+	end
+	% find the maximum prediction probability for each test image
+	mx = max( predProb, [], 2 ); % max probability in each row
+	% prediction class for each test image with tie breaker
+	% only keep the first maximum probability
+	predClass = tieBreaker(bsxfun(@eq, predProb,mx));
+end
+
+function tieBreakVector = tieBreaker(inputVector)
+	tieBreakVector = inputVector;
+	for i = 1 : size(tieBreakVector,1)
+		rowSum = sum(tieBreakVector(i,:));
+		if  rowSum > 1
+			for j = 1 : length(tieBreakVector(i,:))
+				if tieBreakVector(i,length(tieBreakVector(i,:)) - j + 1) ~= 0
+					tieBreakVector(i,length(tieBreakVector(i,:)) - j + 1) = 0;
+					rowSum = rowSum - 1;
+					if rowSum == 1
+						break;
+					end
+				end
+			end
+		end
+	end
 end
--- SpatialPyramid/test.m
+++ SpatialPyramid/test.m
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
+
 % matrix = [0 1 4 2; 1 0 2 3; 4 2 0 1; 2 3 1 0];
 % categoryNameHierarchy = hierarchyFromMatrix(matrix, {'A', 'B', 'C', 'D'});
 % save('../data/GriffinHierarchy.mat', 'categoryNameHierarchy');
 % buildClassifierHierarchy(categoryNameHierarchy);
-=======
-A = [1 0 0; 0 1 0; 1 0 0; 0 0 1; 1 0 0; 0 1 0]
-B = [1 0 0; 1 0 0; 1 0 0; 0 0 1; 1 0 0; 1 0 0]
 
-A & B
-sum(sum(A&B)) / (length(A) + length(B))
->>>>>>> 086c83b761354cb6f36534f792e7bac76e30e854
+k = {'owl-ostrich-duck-goose', 'owl-ostrich', 'duck-goose'};
+v = {{'owl-ostrich', 'duck-goose'}, {'owl', 'ostrich'}, {'duck', 'goose'}};
+nameHierarchy = containers.Map(k,v);
+evaluateHierarchy(nameHierarchy);
+% hierarchy2String(nameHierarchy);
+
+
